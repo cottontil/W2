4ed0607ffde28d6307cb6523ab55855090afa647
Aaron Ye <ajy33@cornell.edu>
2023-01-17 21:41:43 -0800

    removed unziped file

--- mac/HardwareFiles/1.1.0/cores/arduino/Serial.h
+++
@@ -1,9 +0,0 @@
-void SetBaudRate(unsigned long buadRate);
-
-void Putch(char ch);
-void Puts(char const* str);
-
-char* itoa(int num, char* str, int radix);
-int atoi(const char* str);
-
-char ReadChar();
